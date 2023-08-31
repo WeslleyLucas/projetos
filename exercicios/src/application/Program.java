22390dd876a6826d1ac4b5bf2fe7d4ac607db323
Weslley Lucas <Weslley Lucas@WeslleyLucas>
2023-08-31 10:37:40 -0300

    Exercício de criação de conta bancária de usuário.

---
+++ exercicios/src/application/Program.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Account;
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Account account; 
+		
+		System.out.println("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.println("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.println("Is there an initial deposit? (y/n) ");
+		char response = sc.next().charAt(0);
+		
+		if(response == 'y') {
+			System.out.println("Enter initial deposit amount: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);
+		}
+		else { 
+			account = new Account(number, holder);
+		}
+		
+		System.out.println("Account data: ");
+		System.out.println(account.toString());
+		System.out.println();
+		
+		System.out.print("Enter deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		
+		System.out.println("Updated Account Data: ");
+		System.out.println(account);
+		System.out.println();
+		
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		
+		System.out.println("Updated Data: ");
+		System.out.println(account);
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
